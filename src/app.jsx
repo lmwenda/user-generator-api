e916487131f2cf4e173e346d082e0ad43e91c4a5
Trafalgar Law <67679722+lmwenda@users.noreply.github.com>
2020-12-08 23:20:26 +0000

    Create app.jsx

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import App from './Components/App';
+
+function Main() {
+  return (
+    <div className="App">
+      <App />
+    </div>
+  );
+}
+
+export default Main;
